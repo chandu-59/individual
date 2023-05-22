4b72b21ba2cbfa8c55a664d9d31c353c5ee4b194
chandu-59 <chandukotaiiitdwd@gmail.com>
2023-05-22 17:47:54 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import find_packages,setup
 from typing import List
 
+HYPEN_E_DOT = '-e .'
+
 def get_req(a:str)->List[str]:
     requirements=[]
     with open(a) as file_obj:
         reuirements=file_obj.readlines()
         [i.replace("/n","") for i in requirements]
         
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+    
 setup(name='MLproject',
 versio='0.0.1',
 author='chandu',
